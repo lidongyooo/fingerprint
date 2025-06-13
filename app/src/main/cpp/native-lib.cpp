604e43e4e1cbe89e48f79a39d90db4ac822e5f02
lidongyooo <2271439559@qq.com>
2025-06-13 22:04:41 +0800

    frida check

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,11 @@
 #include <jni.h>
 #include <string>
+#include "netinet/in.h"
+#include "arpa/inet.h"
+#include "unistd.h"
+#include "android/log.h"
+
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "fingerprintnative", __VA_ARGS__);
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_lidongyooo_fingerprint_MainActivity_stringFromJNI(
@@ -7,4 +13,61 @@ Java_com_lidongyooo_fingerprint_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT jboolean JNICALL Java_com_lidongyooo_fingerprint_MainActivity_fridaPortCheck(JNIEnv *env, jobject /*this*/, jint port) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        LOGE("socket() failed with errno: %d (%s)", errno, strerror(errno)); // 👈 关
+        return JNI_FALSE;
+    }
+
+    struct sockaddr_in sa{};
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port);
+    if (inet_aton("127.0.0.1", &sa.sin_addr) <= 0) {
+        close(sock);
+        return JNI_FALSE;
+    }
+
+    int result = connect(sock, reinterpret_cast<struct sockaddr *>(&sa), sizeof(sa));
+    close(sock);
+    return (result == 0) ? JNI_TRUE : JNI_FALSE;
+}
+
+
+extern "C"
+JNIEXPORT jboolean JNICALL Java_com_lidongyooo_fingerprint_MainActivity_fridaMapsCheck(JNIEnv *env, jobject /*this*/) {
+    char line[512];
+    FILE *fp;
+    fp = fopen("/proc/self/maps", "r");
+    if (fp) {
+        while (fgets(line, 512, fp)) {
+            LOGE("%s", line);
+            if (strstr(line, "frida")) {
+                return JNI_TRUE;
+            }
+        }
+        fclose(fp);
+    }
+
+    return JNI_FALSE;
+}
+
+extern "C"
+JNIEXPORT jboolean JNICALL Java_com_lidongyooo_fingerprint_MainActivity_fridaCheck(JNIEnv *env, jobject /*this*/) {
+    char line[512];
+    FILE *fp;
+    fp = fopen("/proc/self/maps", "r");
+    if (fp) {
+        while (fgets(line, 512, fp)) {
+            if (strstr(line, "frida")) {
+                return JNI_TRUE;
+            }
+        }
+        fclose(fp);
+    }
+
+    return JNI_FALSE;
 }
--- app/src/main/java/com/lidongyooo/fingerprint/MainActivity.java
+++ app/src/main/java/com/lidongyooo/fingerprint/MainActivity.java
@@ -3,6 +3,9 @@ package com.lidongyooo.fingerprint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.lidongyooo.fingerprint.databinding.ActivityMainBinding;
@@ -26,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
+        boolean result = fridaPortCheck(27042); // 确保返回 boolean，不是 Boolean
+        Log.e("fingerprintxxx", String.valueOf(result));
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                boolean isFridaDetected = fridaMapsCheck();
+                Log.e("fingerprintxxx", String.valueOf(isFridaDetected));
+            }
+        }, 3000); // 3000 毫秒 = 3 秒
     }
 
     /**
@@ -33,4 +45,8 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    public native boolean fridaPortCheck(int port);
+
+    public native boolean fridaMapsCheck();
 }
